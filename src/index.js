1f4e9f65958382691b0ee4a49e576ba0097689d2
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-08-31 11:34:10 -0700

    Template Literals

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const food = ["Bacon", "Jamon", "Noodles"];
+const fname = "Eric";
+const lname = "S";
 
+const number = Math.floor(Math.random() * 10);
+const food = ["Bacon", "Jamon", "Noodles"];
 const listFood = food.map((food) => <li>{food}</li>);
 
 ReactDom.render(
   <div>
     <h1>My Favorite Foods!</h1>
     <ul>{listFood}</ul>
+
+    <p>Your lucky number is {number}</p>
+    <p>My name is {fname + " " + lname}</p>
   </div>,
   document.getElementById("root")
 );
